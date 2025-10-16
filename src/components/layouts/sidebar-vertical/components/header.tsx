c9ceaf5513a6de0b15fdb4be80413ee9014ec028
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2025-10-16 13:43:24 +0700

    fix: :bug: fix cannot apply token for other theme light mode

--- src/components/layouts/sidebar-vertical/components/header.tsx
+++ src/components/layouts/sidebar-vertical/components/header.tsx
@@ -7,7 +7,7 @@ export function Header() {
   const { isMobile } = useLayout();
 
   return (
-    <header className="bg-white border-border supports-backdrop-filter:bg-white fixed start-0 end-0 top-0 z-10 flex h-(--header-height-mobile) shrink-0 items-stretch border-b pe-[var(--removed-body-scroll-bar-size,0px)] lg:h-(--header-height)">
+    <header className="bg-header-background border-border supports-backdrop-filter:bg-header-background fixed start-0 end-0 top-0 z-10 flex h-(--header-height-mobile) shrink-0 items-stretch border-b pe-[var(--removed-body-scroll-bar-size,0px)] lg:h-(--header-height)">
       <div className="@container flex grow items-stretch justify-between gap-2.5 lg:gap-0 pe-5">
         <div className="flex items-stretch gap-x-6">
           <HeaderLogo />
